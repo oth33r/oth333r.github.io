4345a9f4a5b7c21a201f9eb4577042f7241c67df
oth33r <108088389+oth33r@users.noreply.github.com>
2023-07-12 13:58:07 +0300

    fixed

--- src/lib/Comic.svelte
+++ src/lib/Comic.svelte
@@ -1,52 +1,52 @@
 <script lang="ts">
-	import type { ComicInfo } from './index';
+	// import type { ComicInfo } from './index';
 
-	export var comic: ComicInfo;
+	// export var comic: ComicInfo;
 
-	import { onMount } from 'svelte';
-	import moment from 'moment';
-	onMount(() => {
-		if (typeof document !== 'undefined') {
-			interface Comic {
-				month: string;
-				url: string;
-				year: string;
-				safe_title: string;
-				alt: string;
-				img: string;
-				day: string;
-			}
+	// import { onMount } from 'svelte';
+	// import moment from 'moment';
+	// onMount(() => {
+	// 	if (typeof document !== 'undefined') {
+	// 		interface Comic {
+	// 			month: string;
+	// 			url: string;
+	// 			year: string;
+	// 			safe_title: string;
+	// 			alt: string;
+	// 			img: string;
+	// 			day: string;
+	// 		}
 
-			async function fetchXKCDIdentifier(email: string): Promise<string> {
-				const params = new URLSearchParams();
-				if (email) {
-					params.append('email', email);
-				}
+	// 		async function fetchXKCDIdentifier(email: string): Promise<string> {
+	// 			const params = new URLSearchParams();
+	// 			if (email) {
+	// 				params.append('email', email);
+	// 			}
 
-				const r = await fetch('https://fwd.innopolis.university/api/hw2?' + params.toString());
-				return await r.json();
-			};
+	// 			const r = await fetch('https://fwd.innopolis.university/api/hw2?' + params.toString());
+	// 			return await r.json();
+	// 		}
 
-			async function fetchInfo(id: string): Promise<Comic> {
-				const params = new URLSearchParams();
-				if (id) {
-					params.append('id', id);
-				}
+	// 		async function fetchInfo(id: string): Promise<Comic> {
+	// 			const params = new URLSearchParams();
+	// 			if (id) {
+	// 				params.append('id', id);
+	// 			}
 
-				const r = await fetch('https://fwd.innopolis.university/api/comic?' + params.toString());
-				return await r.json();
-			};
-			const func = async () => {
-				const identifier = await fetchXKCDIdentifier('i.zubkov@innopolis.university');
-				const info: Comic = await fetchInfo(identifier);
-				const date = moment(`${info.year}-${info.month}-${info.day}`).fromNow();
-				comic.safe_title = info.safe_title;
-				comic.dataInfo = date;
-			};
+	// 			const r = await fetch('https://fwd.innopolis.university/api/comic?' + params.toString());
+	// 			return await r.json();
+	// 		}
+	// 		const func = async () => {
+	// 			const identifier = await fetchXKCDIdentifier('i.zubkov@innopolis.university');
+	// 			const info: Comic = await fetchInfo(identifier);
+	// 			const date = moment(`${info.year}-${info.month}-${info.day}`).fromNow();
+	// 			comic.safe_title = info.safe_title;
+	// 			comic.dataInfo = date;
+	// 		};
 
-			func();
-		}
-	});
+	// 		func();
+	// 	}
+	// });
 </script>
 
 <div class="comic1" id="comic">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,9 @@
 <script src="src/lib/index.ts" lang="ts">
 	import Main from '$lib/Main.svelte';
 	import Comic from '$lib/Comic.svelte';
-
-	import type { ComicInfo } from '$lib/index';
-
-	let comic: ComicInfo;
 </script>
 
 <main>
 	<Main />
-	<Comic {comic} />
+	<Comic />
 </main>
